f41ed976d9294f2d35f9da815725c393996d469a
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-10 16:03:34 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,64 @@
+//1. Primitive:-  they are 7 type - 1.String, 2.Number, 3.Undefind, 4.Boolean, 5.Bigint, 6.Null ,7.Symbol
+//2. Non-Primitive-   1.Array, 2.Object, 3.Functions
+
+
+// //String
+// let name = "Nandani"
+// console.log(name);
+// console.log(typeof(name))
+
+
+// //Number
+// let num = 2+2;
+// console.log(num)
+// console.log(typeof(num))
+
+// //Undefind
+// let und ;
+// console.log(und)
+// console.log(typeof(und))
+
+// //Boolean
+// let bool = 1
+// // let bool = true
+// console.log(bool)
+// console.log(typeof(bool))
+
+
+// //Bigint
+// let number = 21543652376324572n;
+// console.log(number)
+// console.log(typeof(number))
+
+
+// //Null
+
+// let temp = null;
+// console.log(null)
+// console.log(typeof(null))
+
+
+
+// //Array
+
+// const arr = ["ram", "Seeta", 1, 0.5]
+// console.log(arr)
+// console.log(typeof(arr))
+
+// //Object
+
+// let obj = {
+//     name : "Nandani",
+//     age :21
+// }
+// console.log(obj)
+// console.log(typeof(obj))
+
+
+// // Function
+
+// var myFunction = function()
+// {
+//  console.log("hello")
+// }
+// console.log(typeof(myFunction))
