c2d4a412902562c11102ff776690d4095a29ecd4
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-17 19:28:16 +0530

    scope in js

--- restoprator.js
+++ restoprator.js
@@ -1,4 +1,5 @@
 function calculateCart(...num1) {
+// function calculateCart(val1,val2, ...num1) {
   return num1;
 }
-console.log(calculateCart(4, 6, 9));
+console.log(calculateCart(4, 6, 9,10));
---
+++ scope.js
@@ -0,0 +1,11 @@
+var a = 50;
+let b = 60;
+if (true) {
+  //   var a = 5;
+  let b = 6;
+  const c = 7;
+  console.log("Value of Inner let : ", b);
+}
+// console.log(a);
+console.log(b);
+// console.log(c);
