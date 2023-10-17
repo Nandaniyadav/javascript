9b0ac6f418a445302997c4cbb0ceed4fd7e879fa
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-17 20:01:59 +0530

    learn scope

---
+++ scope2.js
@@ -0,0 +1,20 @@
+// function one() {
+//   const userName = "Nandani Yadav";
+//   function two() {
+//     const website = "Youtube";
+//     console.log(userName);
+//   }
+//   //console.log(website);
+//   two();
+// }
+// one();
+
+// function addOne(num1) {
+//   return num1 + 1;
+// }
+// console.log(addOne(5));
+
+const addTwo = function (num2) {
+  return num2 + 2;
+};
+console.log(addTwo(5));
