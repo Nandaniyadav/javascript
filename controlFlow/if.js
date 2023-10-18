2d2f18842e87c4240d927d69eced09cb26882e8f
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-18 12:25:59 +0530

    if if-else switch and check truthy value

--- controlFlow/if.js
+++ controlFlow/if.js
@@ -1,7 +1,24 @@
-const temprature = 51;
+// const temprature = 51;
 
-if (temprature <= 50) {
-  console.log("Temprature is less than 50");
+// if (temprature <= 50) {
+//   console.log("Temprature is less than 50");
+// } else {
+//   console.log("Temprature is greater than 50");
+// }
+
+// const score = 200;
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`user Power : ${power}`);
+// }
+// console.log(`user Power : ${power}`);
+
+
+const userLogin = true;
+const debidCard = false;
+// const debidCard = true;
+if (userLogin && debidCard == true) {
+  console.log("Allow to Shopping");
 } else {
-  console.log("Temprature is greater than 50");
+  console.log("Not allow for Shopping");
 }
---
+++ controlFlow/truthy.js
@@ -0,0 +1,12 @@
+// const userEmail = "nandani@ai.com";
+// const userEmail = "";
+const userEmail = [];
+if (userEmail) {
+  console.log("Got User Email");
+} else {
+  console.log("not got UserEmail");
+}
+
+// falsy value --> false, 0, -0, Bigint(0n), "", null, undefind, NaN
+
+//truthy value--> "0", "false", " ",[], {}, function(){}
