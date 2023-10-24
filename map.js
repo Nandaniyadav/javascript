97150528cd474ae189cd7e6ebdfcad652d1840c2
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-25 01:18:58 +0530

    map method

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 let arr = [1, 2, 3]
 let newArr = arr.map((value,index,array) => {
   console.log(value,index,array);
-  return index + 1;
+  return value + 1;
+//   return value + index;
 });
 console.log(newArr);
