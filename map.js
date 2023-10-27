8255c674c703808da5e8e105b271814c58d1d9bb
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-27 18:02:11 +0530

    chaining in map method

--- map.js
+++ map.js
@@ -1,7 +1,26 @@
-let arr = [1, 2, 3]
-let newArr = arr.map((value,index,array) => {
-  console.log(value,index,array);
-  return value + 1;
-//   return value + index;
-});
-console.log(newArr);
+// let arr = [1, 2, 3]
+// let newArr = arr.map((value,index,array) => {
+//   //console.log(value,index,array);
+//   console.log(value);
+//   return value + 1;
+// //   return value + index;
+// });
+// console.log(newArr);
+/////
+// const myNums = [1, 2, 3, 4, 5, 6];
+// // const newNums = myNums.map((num)=> num + 2)
+// const newNums = myNums.map((num) => {
+//   return num + 2;
+// });
+// console.log(newNums);
+
+/////
+//chaining in map() method
+const myArr = [1, 2, 3, 4, 5, 6, 7];
+
+const myNewArr = myArr
+                       .map((num) => num * 10)
+                       .map((num) => num + 1)
+                       .map((num)=>  num - 2)
+                      //  .map((num)=>  num / 2);
+console.log(myNewArr);
