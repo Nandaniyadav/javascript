88ed7ca87de2e05c9d75a227a74b1a98568dd839
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-11-06 16:34:01 +0530

    learn reduce method

--- reduce.js
+++ reduce.js
@@ -1,7 +1,14 @@
-var obj = {
-  msg: "Susbribe my channel",
-  printmsg: function(){
-    console.log(this.msg);
-  }
-};
-console.log(obj.msg)
+// var obj = {
+//   msg: "Susbribe my channel",
+//   printmsg: function(){
+//     console.log(this.msg);
+//   }
+// };
+// console.log(obj.msg)
+
+let price = [500, 1000, 7000, 300];
+let newPrice = price.reduce(function (acc, item) {
+  result = acc + item;
+  return result;
+}, 0);
+console.log(result);
