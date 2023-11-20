ae46797ef337b41f1c920f5659d61db55595fe33
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-11-20 23:17:21 +0530

    check even and odd number

---
+++ even.js
@@ -0,0 +1,12 @@
+//Print 1 to 100 all even and odd number
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
