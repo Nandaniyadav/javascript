60f0138157306ad1ceb1973c91b91394cba971ec
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-11-21 17:17:50 +0530

    learn string property

--- string.js
+++ string.js
@@ -1,6 +1,14 @@
-const name = "Nandani";
-const num = 100;
+// const name = "Nandani";
+// const num = 100;
 
-// console.log("My Name is" + name, "My number in", + num)
+// // console.log("My Name is" + name, "My number in", + num)
 
-console.log(`My Name is ${name} and My number is ${num}`);
+// console.log(`My Name is ${name} and My number is ${num}`);
+
+/////////////////////////////////////
+
+let str = "Nandani"
+console.log(str)
+console.log(str.length)
+console.log(str[0],str[1])
+console.log(`Length of Nandani = ${str.length}`)
