ac5f07d3bd94010f74635c5f3b347b9bf962026f
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-11-23 09:40:23 +0530

    find square

--- task/square.js
+++ task/square.js
@@ -1,2 +1,7 @@
 // For a given array of numbers, print the square of each value using the forEach loop.
 
+let nums = [1, 2, 3, 4, 5];
+nums.forEach((nums) => {
+  //   console.log(nums * nums);
+  console.log(`Square of ${nums} is ${nums ** 2}`);
+});
